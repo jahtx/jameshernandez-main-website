bd0e8e6489973734e00a6f1d5e368159d5f1c58f
James Hernandez <main@jahtx.com>
2022-09-14 18:12:55 -0500

    design refinement

--- src/layouts/MainLayout/TitleBit.tsx
+++ src/layouts/MainLayout/TitleBit.tsx
@@ -1,14 +1,16 @@
 import * as React from "react";
 import Container from "react-bootstrap/Container";
 import SouthTejasLogo from "../../assets/south-tejas-logo.svg";
+import SouthTejasLogoDesktop from "../../assets/south-tejas-logo-desktop.svg";
 
 import "./TitleBit.scss";
 
 const TitleBit = () => {
   return (
     <div className="titleBit">
-      <Container className="d-flex justify-content-center">
+      <Container className="titleBit__cont d-flex justify-content-center">
         <SouthTejasLogo />
+        <SouthTejasLogoDesktop />
       </Container>
     </div>
   );
