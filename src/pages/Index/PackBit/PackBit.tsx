737b959cf9dd7e2c1bca7822512c6e3b80261010
James Hernandez <main@jahtx.com>
2022-09-14 23:49:14 -0500

    added imgages to docket and switch function

--- src/pages/Index/PackBit/PackBit.tsx
+++ src/pages/Index/PackBit/PackBit.tsx
@@ -4,14 +4,71 @@ import infoData from "./articleListing.json";
 import { nanoid } from "nanoid";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import { graphql, useStaticQuery } from "gatsby";
+import "./PackBit.scss";
 
 type ArticleType = {
   articleName: string;
   url: string;
+  image: string;
   lead: string;
 };
 
 const PackBit = () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        processImg: file(relativePath: { eq: "items/process.png" }) {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 85
+              width: 400
+              placeholder: BLURRED
+              formats: [WEBP]
+              layout: CONSTRAINED
+            )
+          }
+        }
+        wireframesImg: file(
+          relativePath: { eq: "items/wireframes-yellow.png" }
+        ) {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 85
+              width: 400
+              placeholder: BLURRED
+              formats: [WEBP]
+              layout: CONSTRAINED
+            )
+          }
+        }
+        ipadImg: file(relativePath: { eq: "items/ipad.png" }) {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 85
+              width: 400
+              placeholder: BLURRED
+              formats: [WEBP]
+              layout: CONSTRAINED
+            )
+          }
+        }
+      }
+    `
+  );
+
+  const renderSwitch = (param) => {
+    switch (param) {
+      case "process":
+        return data.processImg;
+        break;
+      case "screens":
+        return data.wireframesImg;
+        break;
+      case "ipad":
+        return data.ipadImg;
+    }
+  };
+
   const articles: Array<ArticleType> = infoData.articles;
   return (
     <section className="packBit">
@@ -19,12 +76,19 @@ const PackBit = () => {
         <ul className="docket">
           {articles &&
             articles.map((article) => {
-              const { articleName, url, lead } = article;
+              const { articleName, url, lead, image } = article;
               const id = nanoid();
               return (
-                <li className="docketItem">
+                <li key={id} className="docketItem">
                   <a href={url}>
                     <h3 className="title">{articleName}</h3>
+                    <div className="text-center">
+                      <GatsbyImage
+                        className="docket__mainImg"
+                        image={getImage(renderSwitch(image))}
+                        alt="alt"
+                      />
+                    </div>
                     <div className="docketItem__descrip">{lead}</div>
                   </a>
                 </li>
