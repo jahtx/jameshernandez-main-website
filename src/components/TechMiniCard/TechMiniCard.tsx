af8cc1956dac5ac32a12adc2c2f45cf57335851f
James Hernandez <main@jahtx.com>
2022-09-21 22:39:31 -0500

    go?

--- src/components/TechMiniCard/TechMiniCard.tsx
+++ src/components/TechMiniCard/TechMiniCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { graphql, useStaticQuery } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
-import "./techminicard.scss";
+import "./TechMiniCard.scss";
 
 /**
  *
