1066704b518267a66af09939d250630efe742cfd
James Hernandez <main@jahtx.com>
2022-09-22 13:43:32 -0500

    go

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -23,42 +23,65 @@ const resumePage = () => {
             })}
         </ul>
         <h2>Experience</h2>
-        {resumeData.work &&
-          resumeData.work.map((workItem) => {
-            const { summary, clients, name, technologies } = workItem;
-            const id = nanoid();
+        <div>
+          {resumeData.work &&
+            resumeData.work.map((workItem) => {
+              const { summary, clients, name, position, technologies } =
+                workItem;
+              const id = nanoid();
 
-            return (
-              <div key={id} className="mt-3">
-                <h3>{name}</h3>
-                <div>
-                  {clients ? null : (
-                    <>
-                      {summary}
-                      <br />
-                      {technologies && <TechUsed technologies={technologies} />}
-                    </>
-                  )}
+              return (
+                <div key={id} className="job mt-3">
+                  <h3>{position}</h3>
+                  <h4>{name}</h4>
+                  <div>
+                    {clients ? null : (
+                      <>
+                        {summary}
+                        <br />
+                        {technologies && (
+                          <TechUsed technologies={technologies} />
+                        )}
+                      </>
+                    )}
+                  </div>
+                  {/* div created to contain all the clients */}
+                  <div>
+                    {clients &&
+                      clients.map((client) => {
+                        const { name, summary, technologies } = client;
+                        const id = nanoid();
+                        return (
+                          <div className="clientSection" key={id}>
+                            <h4>Client: {name}</h4>
+                            <div>{summary}</div>
+                            {technologies && (
+                              <TechUsed technologies={technologies} />
+                            )}
+                          </div>
+                        );
+                      })}
+                  </div>
                 </div>
-                <div>
-                  {clients &&
-                    clients.map((client) => {
-                      const { name, summary, technologies } = client;
-                      const id = nanoid();
-                      return (
-                        <div className="clientSection" key={id}>
-                          <h4>Client: {name}</h4>
-                          <div>{summary}</div>
-                          {technologies && (
-                            <TechUsed technologies={technologies} />
-                          )}
-                        </div>
-                      );
-                    })}
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
+        </div>
+        <h4>Operating Systems & Applications</h4>
+        <ul>
+          {resumeData.basics.operatingSystems &&
+            resumeData.basics.operatingSystems.map((app) => {
+              const id = nanoid();
+              return <li key={id}>{app}</li>;
+            })}
+        </ul>
+        <h4>Languages and Technologies</h4>
+        <ul>
+          {resumeData.basics.languagesAndTech &&
+            resumeData.basics.languagesAndTech.map((tech) => {
+              const id = nanoid();
+              return <li key={id}>{tech}</li>;
+            })}
+        </ul>
       </Container>
     </MainLayout>
   );
