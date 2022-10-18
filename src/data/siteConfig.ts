b9218eca0fdc396088a31d8435dc60b1a2331f6c
James Hernandez <main@jahtx.com>
2022-10-17 19:38:32 -0500

    go

--- src/data/siteConfig.ts
+++ src/data/siteConfig.ts
@@ -7,6 +7,7 @@ const siteConfig = {
   description:
     "a user experience and web development business in the South Texas area",
   googleAnalyticsID: "G-VFZEZQXCGB",
+  copyright: "Copyright © 2022, James Hernandez, All Rights Reserved",
 };
 
 export default siteConfig;
--- src/layouts/MainLayout/Footer.tsx
+++ src/layouts/MainLayout/Footer.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
+import config from "../../data/siteConfig";
 import "./Footer.scss";
 
 const Footer = () => {
+  const { copyright } = config;
+  if (!copyright) {
+    return null;
+  }
   return (
     <footer className="footer text-center pt-7-rem pt-3 pb-3">
-      <span className="footer__text">
-        Copyright &copy; 2022, James Hernandez, All Rights Reserved
-      </span>
+      <span className="footer__text">{copyright}</span>
     </footer>
   );
 };
