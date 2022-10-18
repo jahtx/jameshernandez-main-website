2c4e5c9461a0eb5b8de711660d08d4aa0234dc6a
James Hernandez <main@jahtx.com>
2022-10-17 19:22:55 -0500

    fixed again

--- src/pages/portfolio/rbfcu.tsx
+++ src/pages/portfolio/rbfcu.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { graphql, useStaticQuery, HeadFC } from "gatsby";
 import { nanoid } from "nanoid";
-import { RbImageQuery } from "./RbImageQuery";
+
 import Container from "react-bootstrap/Container";
 import { getImage } from "gatsby-plugin-image";
 import MainLayout from "../../layouts/MainLayout";
@@ -122,3 +122,114 @@ export const Head: HeadFC = () => (
     </title>
   </>
 );
+
+export const RbImageQuery = () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        mainMobile: file(relativePath: { eq: "composites/RBFCU_Main-1.png" }) {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 85
+              width: 270
+              placeholder: BLURRED
+              formats: [WEBP]
+              layout: CONSTRAINED
+            )
+          }
+        }
+        mainDesktop: file(relativePath: { eq: "composites/RBFCU_Main-2.png" }) {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 85
+              width: 760
+              placeholder: BLURRED
+              formats: [WEBP]
+              layout: CONSTRAINED
+            )
+          }
+        }
+        mortgageMobile: file(
+          relativePath: { eq: "composites/RBFCU_Mortgage-1.png" }
+        ) {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 85
+              width: 270
+              placeholder: BLURRED
+              formats: [WEBP]
+              layout: CONSTRAINED
+            )
+          }
+        }
+
+        mortgageDesktop: file(
+          relativePath: { eq: "composites/RBFCU_Mortgage-2.png" }
+        ) {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 85
+              width: 760
+              placeholder: BLURRED
+              formats: [WEBP]
+              layout: CONSTRAINED
+            )
+          }
+        }
+        autoLoansMobile: file(
+          relativePath: { eq: "composites/RBFCU_Auto_Loans-1.png" }
+        ) {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 85
+              width: 270
+              placeholder: BLURRED
+              formats: [WEBP]
+              layout: CONSTRAINED
+            )
+          }
+        }
+        autoLoansDesktop: file(
+          relativePath: { eq: "composites/RBFCU_Auto_Loans-2.png" }
+        ) {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 85
+              width: 760
+              placeholder: BLURRED
+              formats: [WEBP]
+              layout: CONSTRAINED
+            )
+          }
+        }
+        checkingMobile: file(
+          relativePath: { eq: "composites/RBFCU_Checking-1.png" }
+        ) {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 85
+              width: 270
+              placeholder: BLURRED
+              formats: [WEBP]
+              layout: CONSTRAINED
+            )
+          }
+        }
+        checkingDesktop: file(
+          relativePath: { eq: "composites/RBFCU_Checking-2.png" }
+        ) {
+          childImageSharp {
+            gatsbyImageData(
+              quality: 85
+              width: 760
+              placeholder: BLURRED
+              formats: [WEBP]
+              layout: CONSTRAINED
+            )
+          }
+        }
+      }
+    `
+  );
+  return data;
+};
