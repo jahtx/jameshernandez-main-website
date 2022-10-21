f97487c51ba7b2c73e32e2caf7e719e20cbec44c
James Hernandez <main@jahtx.com>
2022-10-20 20:18:43 -0500

    small shakes back

--- src/components/Index/TagBit/TagBit.tsx
+++ src/components/Index/TagBit/TagBit.tsx
@@ -3,11 +3,10 @@ import Container from "react-bootstrap/Container";
 import lanyardTag from "../../../images/name-tags/lanyardTag.png";
 import steelTag from "../../../images/name-tags/steelTag.png";
 import hookTag from "../../../images/name-tags/hookTag.png";
-
 import "./TagBit.scss";
 
 const TagBit = () => {
-  const [imag, setImag] = useState(null);
+  const [tagimage, setTagimage] = useState(null);
 
   React.useEffect(() => {
     const randImages = [lanyardTag, steelTag, hookTag];
@@ -15,7 +14,7 @@ const TagBit = () => {
     const randomGenerator = () => {
       return Math.floor(Math.random() * randImages.length);
     };
-    setImag(randImages[randomGenerator()]);
+    setTagimage(randImages[randomGenerator()]);
   }, []);
 
   return (
@@ -25,7 +24,11 @@ const TagBit = () => {
           className="crate__first d-flex justify-content-center trans-2sec"
           data-sal="slide-down"
         >
-          <img src={imag} className="nameTagImg" alt="James Hernandez" />
+          <img
+            src={tagimage}
+            className="nameTagImg"
+            alt="James Hernandez Name Tag"
+          />
         </div>
         <div className="crate__second d-flex text-light justify-content-center align-items-center">
           <div className="crate__innerText one-rem m-1">
